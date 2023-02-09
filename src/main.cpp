5ae30b58c29a5e8232e58113088edeaa5667e410
kayachnn <kayachnn@gmail.com>
2023-02-09 23:56:01 +0300

    try sending same packet 3 times and stop when reched the destination

--- src/main.cpp
+++ src/main.cpp
@@ -13,11 +13,6 @@
 
 using namespace std;
 
-#define IP_HEADER_LEN sizeof(iphdr)
-#define ICMP_HEADER_LEN sizeof(icmphdr)
-#define IP_PACKET_LEN IP_HEADER_LEN + ICMP_HEADER_LEN
-
-
 const int BUF_SIZE = 1024;
 const int TIMEOUT = 2;
 
@@ -49,6 +44,8 @@ struct icmp_echo {
 int main(int argc, char *argv[])
 {
     int ttl = 0;
+    int ttlCount=0;
+    int destPort = 33345;
     std::string message = "Hello from UDP";
 
     if (argc != 2) {
@@ -56,12 +53,7 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    // Create a raw socket
-    int sock = socket(AF_INET, SOCK_DGRAM, 0);
-    if (sock < 0) {
-        std::cerr << "Error: Cannot create socket" << std::endl;
-        return 1;
-    }
+    
     int sockIcmp = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
     if(sockIcmp < 0){
         std::cerr << "Error: Cannot create raw socket\n";
@@ -79,28 +71,41 @@ int main(int argc, char *argv[])
     struct sockaddr_in dest;
     bzero(&dest, sizeof(dest));
     dest.sin_family = AF_INET;
-    dest.sin_port = 12345;
+    dest.sin_port = destPort++;
     memcpy(&dest.sin_addr, host->h_addr, host->h_length);
+
+    std::string destIp(INET_ADDRSTRLEN, 0x00);
+    inet_ntop(AF_INET, &dest.sin_addr, (char *)destIp.data(), INET_ADDRSTRLEN);
+    std::cout << "dest: " << destIp << '\n';
 
 
     struct timeval timeout;
-    timeout.tv_sec = 1; // 2 second timeout
-    timeout.tv_usec = 0;
+    timeout.tv_sec = 0; // 2 second timeout
+    timeout.tv_usec = 10;
     if (setsockopt(sockIcmp, SOL_SOCKET, SO_RCVTIMEO, (char *) &timeout, sizeof(timeout)) < 0) {
         std::cerr << "setsockopt error" << std::endl;
         return -1;
     }
-    if (setsockopt(sock, SOL_SOCKET, SO_RCVTIMEO, (char *) &timeout, sizeof(timeout)) < 0) {
-        std::cerr << "setsockopt error" << std::endl;
-        return -1;
-    }
+    
 
     char recv_buf[1024];
     
     // Loop through each hop
     
-    while (ttl < 64)
+    while (ttl < 70)
     {
+        // Create a raw socket
+        int sock = socket(AF_INET, SOCK_DGRAM, 0);
+        if (setsockopt(sock, SOL_SOCKET, SO_RCVTIMEO, (char *) &timeout, sizeof(timeout)) < 0) {
+            std::cerr << "setsockopt error" << std::endl;
+            return -1;
+        }
+        if (sock < 0) {
+            std::cerr << "Error: Cannot create socket" << std::endl;
+            return 1;
+        }
+
+        dest.sin_port = destPort++;
         bzero(recv_buf, sizeof(recv_buf));
         // Increment the TTL
         ttl++;
@@ -117,14 +122,25 @@ int main(int argc, char *argv[])
             return -1;
         }
 
-
+        //chekc if udp response received
 
         bzero(recv_buf, sizeof(recv_buf));
         int recv_len = recv(sockIcmp, recv_buf, sizeof(recv_buf), 0);
         if (recv_len < 0) {
-            std::cerr << "recvfrom error" << std::endl;
-            continue;
+            //std::cerr << "recvfrom error" << std::endl;
+            if(ttlCount < 2){
+                ttl--;
+                ttlCount++;
+                continue;
+            }
+            else{
+                ttlCount = 0;
+                continue;
+            }
+            
+            
         }
+        
         std::cout << "received package\n";
         
 
@@ -132,15 +148,19 @@ int main(int argc, char *argv[])
         struct icmphdr *icmp_header = (struct icmphdr *) (recv_buf + (ip_header->ihl * 4));
 
         // convert source IP address to string
-        char src_ip[INET_ADDRSTRLEN];
-        inet_ntop(AF_INET, &ip_header->saddr, src_ip, INET_ADDRSTRLEN);
-        std::cout << "Received ICMP packet from IP address: " << src_ip << std::endl;
-       
+        std::string srcIp(INET_ADDRSTRLEN, 0x00);
+        inet_ntop(AF_INET, &ip_header->saddr, (char *)srcIp.data(), INET_ADDRSTRLEN);
+        std::cout << "Received ICMP packet from IP address: " << srcIp << std::endl;
+        close(sock);
+
+        if(srcIp == destIp)
+        break;
             
     
 }
 
-close(sock);
+
+close(sockIcmp);
 
 return 0;
 }
